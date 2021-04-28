5efa50e69544f09b5a9d126a605e221a75ea687c
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-04-28 16:01:05 +0200

    baza upd2

--- baza3.py
+++ baza3.py
@@ -2,6 +2,7 @@ import psycopg2
 from PyQt6.QtSql import QSqlDatabase, QSqlTableModel, QSqlQueryModel
 from mainwindow import Ui_MainWindow
 from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtCore import Qt, QSize
 
 connection = psycopg2.connect(host='localhost', user='postgres', password='Nana@1974', port ='5432', database='szkolenie')
 
@@ -28,16 +29,20 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.model = QSqlTableModel(db=db)
         self.tableView.setModel(self.model)
         self.model.setTable("test")
+        #self.model.sort(2,Qt.SortOrder)
+        self.tableView.setSortingEnabled(True)
+        self.tableView.sortByColumn(1, Qt.SortOrder(1))
         self.model.select()
-
+        
     def zapisz_do_bazy(self):
 
         parametr_Name = self.plainTextEdit.toPlainText()
-        # sql_insert = f"""INSERT INTO artists (Name) 
-        #               VALUES ('{parametr_Name}');"""
+        sql_insert = f"""INSERT INTO public.test(
+                            nazwisko, imie)
+                            VALUES ('{parametr_Name}', 'no name');"""
 
-        # wynik = db.exec(sql_insert)
-        # self.model.select()
+        wynik = db.exec(sql_insert)
+        self.model.select()
 
 
 app = QtWidgets.QApplication([])
